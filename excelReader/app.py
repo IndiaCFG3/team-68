2aafadecd18d8d46456f84e646cbc134ae0da1d9
unknown <saxenamanas0@gmail.com>
2020-08-16 17:13:55 +0530

    Added principal dashboard

---
+++ excelReader/app.py
@@ -0,0 +1,9 @@
+import xlrd
+loc = ("./sheet.xlsx") 
+  
+# To open Workbook 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+  
+# For row 0 and column 0 
+print(sheet.cell_value(0, 0))
